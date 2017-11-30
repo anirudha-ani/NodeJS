568dddde9f8b81103c5dcb97cb8762b4a9106bc3
Anirudha Paul <anirudhaprasun@gmail.com>
2017-11-30 22:24:03 +0600

    Episode 3 Finished
    Interesting things about Functions in NodeJS

--- Starting NodeJS/app.js
+++ Starting NodeJS/app.js
@@ -1,1 +1,31 @@
 console.log('bacon');
+
+var person = {
+    firstName: "Anirudha",
+    lastname: "Paul",
+    age: 21
+};
+
+console.log(person);
+
+function addNumber(a , b)
+{
+    return a+b;
+}
+
+console.log(addNumber(4 , 3));
+
+function worthless()
+{
+
+}
+
+console.log(worthless());
+
+var printBacon = function(){
+    console.log("Chicken Rocks");
+};
+
+printBacon();
+
+setTimeout(printBacon , 5000);
